4067d3c65ee05bd2cf1fafd19d5bf59787f6d3ae
Praveen P <160019321+PraveenP9105@users.noreply.github.com>
2026-03-12 09:23:44 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/hospital/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/hospital/backend/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://hms-red-six.vercel.app"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
